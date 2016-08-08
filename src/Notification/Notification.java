92751f712d41bf7139ee6af35a4d3815288d81af
charltonraven <williamscharlton@hotmail.com>
2016-08-08 12:04:47 -0400

    Made Changes to the Instructions to the users

--- src/Notification/Notification.java
+++ src/Notification/Notification.java
@@ -9,6 +9,7 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -27,164 +28,169 @@ import javafx.stage.Stage;
 
 public class Notification extends Application implements EventHandler<ActionEvent> {
 
-	Stage window;
-	Button btnSubmit;
-	Button btnCancel;
+    Stage window;
+    Button btnSubmit;
+    Button btnCancel;
 
-	Label lblAsk;
+    Label lblAsk;
 
-	Label lblColon;
-	Label lblComma;
-	Label lblWarning;
+    Label lblColon;
+    Label lblComma;
+    Label lblWarning;
+    Label lblSteps;
 
-	static ChoiceBox<String> day, hours, minutes, AMorPM;
+    static ChoiceBox<String> day, hours, minutes, AMorPM;
 
-	public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) throws UnknownHostException {
 
-		launch(args);
-	}
+        launch(args);
+    }
 
-	@SuppressWarnings("static-access")
-	@Override
-	public void start(Stage PrimaryStage) throws Exception {
+    @SuppressWarnings("static-access")
+    @Override
+    public void start(Stage PrimaryStage) throws Exception {
 
-		window = PrimaryStage;
-		window.setTitle("Data Transfer Notification");
+        window = PrimaryStage;
+        window.setTitle("Data Transfer Notification");
 
-		// button
-		btnSubmit = new Button("Submit");
-		btnCancel = new Button("Cancel");
+        // button
+        btnSubmit = new Button("Submit");
+        btnCancel = new Button("Cancel");
 
-		// change button size
-		btnSubmit.setStyle("-fx-font-size: 13pt;");
-		btnCancel.setStyle("-fx-font-size: 13pt;");
+        // change button size
+        btnSubmit.setStyle("-fx-font-size: 13pt;");
+        btnCancel.setStyle("-fx-font-size: 13pt;");
 
-		// Layout Gridview
-		GridPane grid = new GridPane();
-		grid.setPadding(new Insets(5, 5, 5, 5));
-		grid.setHgap(5);
-		grid.setVgap(10);
+        // Layout Gridview
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(5, 5, 5, 5));
+        grid.setHgap(5);
+        grid.setVgap(10);
 
-		// Labels
-		lblAsk = new Label("Schedule Your Data Transfer");
-		// lblTime = new Label("Schedule a time for your Data Transfer: ");
-		lblColon = new Label(":");
-		lblComma = new Label(",");
-		lblWarning = new Label(
-				"**** Work days are Monday-Friday from 8AM to 5PM *****\n\nIf you are not ready at this time please give us a call at 843-383-7050");
+        // Labels
+        lblAsk = new Label("Schedule Your Data Transfer");
+        // lblTime = new Label("Schedule a time for your Data Transfer: ");
+        lblColon = new Label(":");
+        lblComma = new Label(",");
+        lblSteps = new Label("DO THE FOLLOWING PRIOR TO THE TIME OF THE DATA TRANSFER:\n1). Make sure the old and new PCs are powered on.\n2). Be sure that laptops have power adapters plugged in.\n3).Connect both of them to the network via network cable.");
+        lblWarning = new Label(
+                "**** Work days are Monday-Friday from 8AM to 5PM *****\n\nIf you would like to schedule your Data transfer on another week \nor\nIf you have problems connecting your PC to the network.\n\n*****Give us a call, 843-383-7050.");
+        lblWarning.setStyle("-fx-font-size: 9");
+        lblWarning.wrapTextProperty();
 
-		// label Font
-		lblAsk.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
-		lblWarning.setFont(Font.font("Verdana", 9));
-		lblWarning.setTextFill(Color.web("#ff0000"));
+        // label Font
+        lblAsk.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
+        lblWarning.setFont(Font.font("Verdana", 9));
+        lblWarning.setTextFill(Color.web("#ff0000"));
 
-		// Drop down List INCLUDING A HBOX...THIS PUTS THEM ON THE SAME LINE.
-		// AM or PM
-		AMorPM = new ChoiceBox<>();
-		AMorPM.getItems().addAll("A.M.", "P.M.");
+        // Drop down List INCLUDING A HBOX...THIS PUTS THEM ON THE SAME LINE.
+        // AM or PM
+        AMorPM = new ChoiceBox<>();
+        AMorPM.getItems().addAll("A.M.", "P.M.");
 
-		// Hours and Minutes
-		day = new ChoiceBox<>();
-		hours = new ChoiceBox<>();
-		minutes = new ChoiceBox<>();
-		hours.getItems().addAll("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
-		day.getItems().addAll("Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
-		int by5 = 0;
+        // Hours and Minutes
+        day = new ChoiceBox<>();
+        hours = new ChoiceBox<>();
+        minutes = new ChoiceBox<>();
+        hours.getItems().addAll("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
+        day.getItems().addAll("Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
+        int by5 = 0;
 
-		for (int i = 1; i <= 12; i++) {
-			if (by5 == 0 || by5 == 5) {
-				minutes.getItems().add("0" + by5);
-				by5 = by5 + 5;
-			} else {
-				minutes.getItems().add(by5 + "");
-				by5 = by5 + 5;
-			}
-		}
+        for (int i = 1; i <= 12; i++) {
+            if (by5 == 0 || by5 == 5) {
+                minutes.getItems().add("0" + by5);
+                by5 = by5 + 5;
+            } else {
+                minutes.getItems().add(by5 + "");
+                by5 = by5 + 5;
+            }
+        }
 
-		// SEt hboxes for the time slots and submit button
-		HBox btnsTimeChoice = new HBox(3);
-		btnsTimeChoice.setMargin(lblColon, new Insets(0, 0, 3, 0));
-		btnsTimeChoice.getChildren().addAll(day, lblComma, hours, lblColon, minutes, AMorPM);
+        // SEt hboxes for the time slots and submit button
+        HBox btnsTimeChoice = new HBox(3);
+        btnsTimeChoice.setMargin(lblColon, new Insets(0, 0, 3, 0));
+        btnsTimeChoice.getChildren().addAll(day, lblComma, hours, lblColon, minutes, AMorPM);
 
-		HBox btnsSubmitCancel = new HBox(8);
-		btnsSubmitCancel.getChildren().addAll(btnSubmit, btnCancel);
+        HBox btnsSubmitCancel = new HBox(8);
+        btnsSubmitCancel.getChildren().addAll(btnSubmit, btnCancel);
 
-		// Place on Grid
+        // Place on Grid
 
-		GridPane.setConstraints(btnsTimeChoice, 0, 1);
-		GridPane.setConstraints(btnsSubmitCancel, 0, 2);
-		GridPane.setConstraints(lblWarning, 0, 3);
+        GridPane.setConstraints(btnsTimeChoice, 0, 1);
+        GridPane.setConstraints(btnsSubmitCancel, 0, 2);
+        GridPane.setConstraints(lblWarning, 0, 4);
+        GridPane.setConstraints(lblSteps,0,3);
 
-		// Buttons
-		btnSubmit.setOnAction(this);
+        // Buttons
+        btnSubmit.setOnAction(this);
 
-		btnCancel.setOnAction(this);
+        btnCancel.setOnAction(this);
 
-		// put on grid
-		grid.getChildren().addAll(lblAsk, btnsTimeChoice, btnsSubmitCancel, lblWarning);
-		Scene scene = new Scene(grid, 350, 200);
+        // put on grid
+        grid.getChildren().addAll(lblAsk, btnsTimeChoice, btnsSubmitCancel, lblWarning,lblSteps);
+        Scene scene = new Scene(grid);
 
-		window.setScene(scene);
-		window.show();
+        window.setScene(scene);
+        window.show();
 
-	}
+    }
 
-	public static void SendEmail() throws UnknownHostException {
+    public static void SendEmail() throws UnknownHostException {
 
-		String Username = System.getProperty("user.name");
-		InetAddress localMachine = InetAddress.getLocalHost();
-		String PCname = localMachine.getHostName();
-		String to = "copssupportws.hartsville@sonoco.com";
-		// String to = "mark.gainey@sonoco.com";
-		String from = "NoReplyDT@sonoco.com";
-		String host = "localhost";
-		Properties properties = System.getProperties();
+        String Username = System.getProperty("user.name");
+        InetAddress localMachine = InetAddress.getLocalHost();
+        String PCname = localMachine.getHostName();
+        String to = "copssupportws.hartsville@sonoco.com";
+        // String to = "mark.gainey@sonoco.com";
+        String from = "NoReplyDT@sonoco.com";
+        String host = "localhost";
+        Properties properties = System.getProperties();
 
-		properties.setProperty("mail.smtp.host", "10.77.48.132");
-		Session session = Session.getDefaultInstance(properties);
-		try {
-			MimeMessage message = new MimeMessage(session);
+        properties.setProperty("mail.smtp.host", "10.77.48.132");
+        Session session = Session.getDefaultInstance(properties);
+        try {
+            MimeMessage message = new MimeMessage(session);
 
-			// Set from: header field of the header
-			message.setFrom(new InternetAddress(from));
-			// set To: header of the head field
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            // Set from: header field of the header
+            message.setFrom(new InternetAddress(from));
+            // set To: header of the head field
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
-			// Set subject: header filed
-			message.setSubject("Signed on user: " + Username + " " + day.getValue() + ", " + hours.getValue() + ":"
-					+ minutes.getValue() + " " + AMorPM.getValue() + " PC Name: " + localMachine
-					+ " (Data Transfer Notification)");
+            // Set subject: header filed
+            message.setSubject("Signed on user: " + Username + " " + day.getValue() + ", " + hours.getValue() + ":"
+                    + minutes.getValue() + " " + AMorPM.getValue() + " PC Name: " + localMachine
+                    + " (Data Transfer Notification)");
 
-			// now set the actual message
-			message.setText("Schedule Data Transfer:\nTime: " + day.getValue() + ", " + hours.getValue() + ":"
-					+ minutes.getValue() + " " + AMorPM.getValue() + "\nSigned On User: " + Username + "\nPC Name: "
-					+ localMachine);
+            // now set the actual message
+            message.setText("Schedule Data Transfer:\nTime: " + day.getValue() + ", " + hours.getValue() + ":"
+                    + minutes.getValue() + " " + AMorPM.getValue() + "\nSigned On User: " + Username + "\nPC Name: "
+                    + localMachine);
 
-			// send message
-			Transport.send(message);
-		} catch (MessagingException mex) {
-			System.out.println(mex);
-		}
-	}
+            // send message
+            Transport.send(message);
+        } catch (MessagingException mex) {
+            System.out.println(mex);
+        }
+    }
 
-	@Override
-	public void handle(ActionEvent event) {
-		if (event.getSource() == btnSubmit) {
-			try {
-				SendEmail();
-			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			event.consume();
-			window.close();
+    @Override
+    public void handle(ActionEvent event) {
+        if (event.getSource() == btnSubmit) {
+            try {
+                SendEmail();
+            } catch (UnknownHostException e) {
 
-		}
-		if (event.getSource() == btnCancel) {
-			event.consume();
-			window.close();
-		}
+                e.printStackTrace();
+            }
+            event.consume();
+            window.close();
 
-	}
+        }
+        if (event.getSource() == btnCancel) {
+            event.consume();
+            window.close();
+        }
+
+    }
 
 }
